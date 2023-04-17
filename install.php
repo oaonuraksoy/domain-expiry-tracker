f2cf0c01d4f23ba4fececec71aed38c8916a1ec7
Onur AKSOY <github@onuraksoy.com.tr>
2023-04-17 13:34:48 +0300

    Update install.php

--- install.php
+++ install.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+  <title>DET INSTALLER</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -17,7 +17,7 @@
 <body class="hold-transition install-page">
 <div>
   <div class="login-logo">
-    <a href="#"><b>Admin</b>LTE</a>
+    <a href="#"><b>DET</b> INSTALLER</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
@@ -31,20 +31,34 @@
           </div>
           <div class="col-7">
             <div class="form-group">
-              <label for="inputName">Name</label>
-              <input type="text" id="inputName" class="form-control">
+              <h4>Veritabanı Bağlantısı</h4>
+              <label for="vtServer">Veri Tabanı Sunucusu</label>
+              <input type="text" id="vtServer" class="form-control" placeholder="localhost" >
             </div>
             <div class="form-group">
-              <label for="inputEmail">E-Mail</label>
-              <input type="email" id="inputEmail" class="form-control">
+              <label for="vtName">Veritabanı Adı <small>Henüz oluşturmadıysanız, biz oluşturacağız.</small></label>
+              <input type="email" id="vtName" class="form-control" placeholder="detDB">
             </div>
             <div class="form-group">
-              <label for="inputSubject">Subject</label>
-              <input type="text" id="inputSubject" class="form-control">
+              <label for="vtUser">Veritabanı Kullanıcı Adı</label>
+              <input type="text" id="vtUser" class="form-control" placeholder="root">
             </div>
             <div class="form-group">
-              <label for="inputMessage">Message</label>
-              <textarea id="inputMessage" class="form-control" rows="4"></textarea>
+              <label for="vtPass">Veritabanı Kullanıcı Şifresi</label>
+              <input type="text" id="vtPass" class="form-control" placeholder="*****">
+            </div>
+            <div class="form-group">
+              <h4>Sisteme Giriş Bilgileri</h4>
+              <label for="Username">E-Posta Adresiniz</label>
+              <input type="mail" id="Username" class="form-control" placeholder="demo@demo.com">
+            </div>
+            <div class="form-group">
+              <label for="password">Şifre Oluştur.</label>
+              <input type="text" id="password" class="form-control" placeholder="******">
+            </div>
+            <div class="form-group">
+              <label for="password2">Tekrar Şifre.</label>
+              <input type="text" id="password2" class="form-control" placeholder="******">
             </div>
             <div class="form-group">
               <input type="submit" class="btn btn-primary" value="Send message">
