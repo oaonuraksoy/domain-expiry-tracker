0abf4925bc8d41ad32ce12ba0e808284e02ca880
Onur AKSOY <github@onuraksoy.com.tr>
2023-04-18 02:50:02 +0300

    Update function.php

--- process/function.php
+++ process/function.php
@@ -5,8 +5,9 @@ if (isset($_POST['Login'])) {
     $login = new Login($_POST['userMail'],  md5($_POST['userPwd'])              );
     if ($login->authenticate()) {
         // Kullanıcı doğrulandı, yönlendirme yapılabilir veya başka bir işlem yapılabilir.        
-        echo "giriş yapıldı";
+      //  echo "giriş yapıldı";
         header('Location: /');
+        exit;
     } else {
         // Kullanıcı doğrulanamadı, hata mesajı gösterilebilir veya başka bir işlem yapılabilir.
         echo "Olmadı bu";
