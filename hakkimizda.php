8d5d473d2b87945eaf1d349eb9d4291f9794aba0
Onur AKSOY <github@onuraksoy.com.tr>
2023-04-18 18:34:41 +0300

    Update hakkimizda.php

--- hakkimizda.php
+++ hakkimizda.php
@@ -5,6 +5,8 @@ require_once('header.php');
 
 <div class="content-wrapper"> 
  <h1>Merhaba Dünya!</h1>
+ <?php $date_str = '18.04.2023';
+echo    $date = date('d-m-Y', strtotime(str_replace('.', '-', $date_str))); ?>
 </div>
 
 
