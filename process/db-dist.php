3ef8b9ba3ddbfce3de5afad25269104ee2f8726c
Onur AKSOY <github@onuraksoy.com.tr>
2023-04-21 00:33:54 +0300

    update

--- process/db-dist.php
+++ process/db-dist.php
@@ -4,4 +4,5 @@ $dbhost='';
 $dbname='';
 $dbuser='';
 $dbpwd='';
+$adminMail='';
 ?>
--- process/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-global $dbhost, $dbname, $dbuser, $dbpwd;
-$dbhost='localhost';
-$dbname='detdb_news4';
-$dbuser='root';
-$dbpwd='';
-$adminMail='smm@onuraksoy.com.tr';
-?>
