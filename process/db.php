e0fb9187685beadab66813c128b6105811a0514e
Onur AKSOY <94720996+oaonuraksoy@users.noreply.github.com>
2024-08-03 12:04:06 +0300

    Update db.php

--- process/db.php
+++ process/db.php
@@ -4,8 +4,8 @@ $dbhost='localhost';
 $dbname='det';
 $dbuser='root';
 $dbpwd='';
-$smtpMail='ik@onuraksoy.com.tr';
-$smtpHost='smtp.yandex.com';
-$smtpPwd='Kqwe123onur..';
-$toMail='ik@onuraksoy.com.tr';
+$smtpMail='ik@ony.c.tr';
+$smtpHost='smp.yaex.om';
+$smtpPwd='Kqur..';
+$toMail='i@onusoy.cotr';
 ?>
